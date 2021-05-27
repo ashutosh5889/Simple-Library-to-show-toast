94292c63fa5c4480d9a54330bccbb76fa92dfe6f
unknown <ashutoshsingh5890@gmail.com>
2021-05-27 16:42:59 +0530

    Second Commit

--- messagetoast/src/main/java/com/example/messagetoast/ToasterMessage.java
+++ messagetoast/src/main/java/com/example/messagetoast/ToasterMessage.java
@@ -1,15 +1,20 @@
 package com.example.messagetoast;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message){
+    public static void s(Context c, String message) {
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public int getImage() {
+
+        return R.drawable.ic_launcher_background;
 
     }
 }
